4e09bd262b2bdc44d5774756645879a401b172e0
bakkeby <bakkeby@gmail.com>
2019-05-24 17:42:26 +0200

    Adding patches, README and personal config

---
+++ README.md
@@ -0,0 +1,55 @@
+My dwm 6.2 build with custom patches. Refer to [https://dwm.suckless.org/](https://dwm.suckless.org/) for details on the dwm window manager, how to install it and how it works.
+
+---
+
+Included patches:
+
+   - [cyclelayouts](patches/dwm-cyclelayouts-6.2.diff)
+      - let's you cycle through all your layouts
+      - useful for demo purposes
+      - refer to [https://dwm.suckless.org/patches/cyclelayouts/](https://dwm.suckless.org/patches/cyclelayouts/)
+
+   - [rotatestack](patches/dwm-rotatestack-6.2.diff)
+      - let's you rotate through the stack
+      - this is especially convenient when mapped to the mouse scrollwheel
+      - refer to [https://dwm.suckless.org/patches/rotatestack/](https://dwm.suckless.org/patches/rotatestack/)
+
+   - [tagallmon](patches/dwm-tagallmon-6.2.diff)
+      - dwm comes with a function `tagmon` that allows you to send a window to an adjacent monitor
+      - this patch replicates the same behaviour, but for all visible windows on the current monitor
+      - let's you _swoosh_ everything to the side before opening something else while reassuring onlookers that yes you do live up to wearing those sunglasses while working
+
+   - [tagswapmon](patches/dwm-tagswapmon-6.2.diff)
+      - similarly to `tagallmon` this patch allows you to swap all visible windows on the current monitor with all visible windows on an adjacent monitor
+      - this may be useful if you have a large main (master) display and one or more smaller secondary (stack) displays
+
+   - [togglefullscreen](patches/dwm-togglefullscreen-6.2.diff)
+      - a simple patch that lets you toggle fullscreen on and off using a single keybinding
+      - this is more practical than you might think as many programs have inconsitent shortcuts for going into fullscreen
+    
+   - [vanitygaps](patches/dwm-vanitygaps-6.2.diff)
+      - there are a few patches out there that add fixed gaps between windows, with or without gaps betwen the screen frame and clients
+      - vanity gaps do this as well, with the difference that the size of inner and outer gaps can be configured separately (as well as the size of horizontal and vertical gaps, respectively)
+      - additionally the size of the gaps can be altered on the fly using keyboard shortcuts (in practice though this is mostly for demo-purposes)
+      - besides vanity reasons (read: unixporn), gaps can also remove a bit of the claustrophobic feel that tiling window managers often impose on new users
+      - includes smartgaps support: whether or not to include outer borders when a single window is open 
+      - includes the option of toggling gaps on and off
+      - includes a series of vanitygap-enabled layouts (nobody should need this many though, the idea being that you remove the ones you don't need):
+         - the default tile layout
+         - [bottomstack](https://dwm.suckless.org/patches/bottomstack/)
+         - [centeredmaster](https://dwm.suckless.org/patches/centeredmaster/)
+         - [deck](https://dwm.suckless.org/patches/deck/)
+         - [fibonacci](https://dwm.suckless.org/patches/fibonacci/)
+         - [gridmode](https://dwm.suckless.org/patches/gridmode/)
+
+---
+
+I have included my personal keybindings (`config.h`) here for reference; feel free to delete this to start fresh with default configuration (from `config.def.h`). Most notable I guess is binding `rotatestack` and `cyclelayouts` to the mouse wheel which can be rather convenient when having many windows in the stack. I also mapped middle-click to move a window to master (`zoom`) rather than toggling floating mode which you enable anyway when dragging a window.
+
+I should also note that to start various applications I have additional keybindings through sxhkd.
+
+---
+
+Example screenshot with exaggerated gaps:
+
+![https://github.com/bakkeby/dwm-vanitygaps/wiki/vanitygaps.jpg](https://github.com/bakkeby/dwm-vanitygaps/wiki/vanitygaps.jpg)
---
+++ config.h
@@ -0,0 +1,165 @@
+/* See LICENSE file for copyright and license details. */
+
+/* appearance */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+static const int showbar            = 1;        /* 0 means no bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
+static const char *fonts[]          = { "monospace:size=10" };
+static const char dmenufont[]       = "monospace:size=10";
+static const char col_gray1[]       = "#222222";
+static const char col_gray2[]       = "#444444";
+static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray4[]       = "#eeeeee";
+static const char col_cyan[]        = "#005577";
+static const char *colors[][3]      = {
+	/*               fg         bg         border   */
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+};
+
+/* tagging */
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
+static const Rule rules[] = {
+	/* xprop(1):
+	 *	WM_CLASS(STRING) = instance, class
+	 *	WM_NAME(STRING) = title
+	 */
+	/* class            instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",           NULL,       NULL,       0,            1,           -1 },
+	{ "Firefox",        NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Google-chrome",  NULL,       NULL,       0,            0,            2 },
+	{ "Slack",          NULL,       NULL,       0,            0,            1 },
+};
+
+/* layout(s) */
+static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const int nmaster     = 1;    /* number of clients in master area */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
+#include "vanitygaps.c"
+
+static const Layout layouts[] = {
+	/* symbol	arrange function */
+	{ "[]☰",	tile },    /* first entry is default */
+	{ "[M]",	monocle },
+	{ "⧉⟧⧠",	NULL },    /* no layout function means floating behavior */
+	{ "=M=",	centeredmaster },
+	{ "⧉M⧠",	centeredfloatingmaster },
+	{ "⚎⚎⚎",	bstack },
+	{ "☰☰☰",	bstackhoriz },
+	{ "𝍖𝍖𝍖",	grid },
+	{ "⟦@⟧",	spiral },
+	{ "⟦➘⟧",	dwindle },
+	{ "D[]",	deck },
+	{ NULL,		NULL },
+};
+
+/* key definitions */
+#define MODKEY Mod4Mask
+#define TAGKEYS(KEY,TAG) \
+	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
+	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+
+/* commands */
+static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *termcmd[]  = { "gnome-terminal", NULL };
+
+static Key keys[] = {
+	/* modifier                     key        function           argument */
+	{ MODKEY,                       XK_d,      spawn,             {.v = dmenucmd } },
+	{ MODKEY,                       XK_Return, spawn,             {.v = termcmd } },
+	{ MODKEY,                       XK_b,      togglebar,         {0} },
+	{ MODKEY,                       XK_j,      focusstack,        {.i = +1 } },
+	{ MODKEY,                       XK_k,      focusstack,        {.i = -1 } },
+	{ MODKEY|Mod1Mask,              XK_j,      rotatestack,       {.i = +1 } },
+	{ MODKEY|Mod1Mask,              XK_k,      rotatestack,       {.i = -1 } },
+	{ MODKEY,                       XK_i,      incnmaster,        {.i = +1 } },
+	{ MODKEY,                       XK_u,      incnmaster,        {.i = -1 } },
+	{ MODKEY,                       XK_h,      setmfact,          {.f = -0.05} },
+	{ MODKEY,                       XK_l,      setmfact,          {.f = +0.05} },
+	{ MODKEY,                       XK_m,      zoom,              {0} },
+	{ MODKEY|Mod1Mask,              XK_u,      incrgaps,          {.i = +1 } },
+	{ MODKEY|Mod1Mask|ShiftMask,    XK_u,      incrgaps,          {.i = -1 } },
+	{ MODKEY|Mod1Mask,              XK_i,      incrigaps,         {.i = +1 } },
+	{ MODKEY|Mod1Mask|ShiftMask,    XK_i,      incrigaps,         {.i = -1 } },
+	{ MODKEY|Mod1Mask,              XK_o,      incrogaps,         {.i = +1 } },
+	{ MODKEY|Mod1Mask|ShiftMask,    XK_o,      incrogaps,         {.i = -1 } },
+	{ MODKEY|Mod1Mask,              XK_6,      incrihgaps,        {.i = +1 } },
+	{ MODKEY|Mod1Mask|ShiftMask,    XK_6,      incrihgaps,        {.i = -1 } },
+	{ MODKEY|Mod1Mask,              XK_7,      incrivgaps,        {.i = +1 } },
+	{ MODKEY|Mod1Mask|ShiftMask,    XK_7,      incrivgaps,        {.i = -1 } },
+	{ MODKEY|Mod1Mask,              XK_8,      incrohgaps,        {.i = +1 } },
+	{ MODKEY|Mod1Mask|ShiftMask,    XK_8,      incrohgaps,        {.i = -1 } },
+	{ MODKEY|Mod1Mask,              XK_9,      incrovgaps,        {.i = +1 } },
+	{ MODKEY|Mod1Mask|ShiftMask,    XK_9,      incrovgaps,        {.i = -1 } },
+	{ MODKEY|Mod1Mask,              XK_0,      togglegaps,        {0} },
+	{ MODKEY|Mod1Mask|ShiftMask,    XK_0,      defaultgaps,       {0} },
+	{ MODKEY,                       XK_Tab,    view,              {0} },
+	{ Mod1Mask,                     XK_Tab,    view,              {0} },
+	{ MODKEY,                       XK_q,      killclient,        {0} },
+	{ MODKEY|Mod1Mask|ControlMask,  XK_1,      setlayout,         {.v = &layouts[0]} },
+	{ MODKEY|Mod1Mask|ControlMask,  XK_2,      setlayout,         {.v = &layouts[1]} },
+	{ MODKEY|Mod1Mask|ControlMask,  XK_3,      setlayout,         {.v = &layouts[2]} },
+	{ MODKEY|Mod1Mask|ControlMask,  XK_4,      setlayout,         {.v = &layouts[3]} },
+	{ MODKEY|Mod1Mask|ControlMask,  XK_5,      setlayout,         {.v = &layouts[4]} },
+	{ MODKEY|Mod1Mask|ControlMask,  XK_6,      setlayout,         {.v = &layouts[5]} },
+	{ MODKEY|Mod1Mask|ControlMask,  XK_7,      setlayout,         {.v = &layouts[6]} },
+	{ MODKEY|Mod1Mask|ControlMask,  XK_8,      setlayout,         {.v = &layouts[7]} },
+	{ MODKEY|Mod1Mask|ControlMask,  XK_9,      setlayout,         {.v = &layouts[8]} },
+	{ MODKEY,                       XK_space,  setlayout,         {0} },
+	{ MODKEY|ShiftMask,             XK_space,  togglefloating,    {0} },
+	{ MODKEY,                       XK_f,      togglefullscreen,  {0} },
+	{ MODKEY,                       XK_0,      view,              {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,      tag,               {.ui = ~0 } },
+	{ MODKEY,                       XK_comma,  focusmon,          {.i = -1 } },
+	{ MODKEY,                       XK_period, focusmon,          {.i = +1 } },
+	{ MODKEY|ControlMask,           XK_comma,  cyclelayout,       {.i = -1 } },
+	{ MODKEY|ControlMask,           XK_period, cyclelayout,       {.i = +1 } },
+	{ MODKEY|Mod1Mask,              XK_comma,  tagmon,            {.i = +1 } },
+	{ MODKEY|Mod1Mask,              XK_period, tagmon,            {.i = -1 } },
+	{ MODKEY|Mod1Mask|ShiftMask,    XK_comma,  tagallmon,         {.i = +1 } },
+	{ MODKEY|Mod1Mask|ShiftMask,    XK_period, tagallmon,         {.i = -1 } },
+	{ MODKEY|Mod1Mask|ControlMask,  XK_comma,  tagswapmon,        {.i = +1 } },
+	{ MODKEY|Mod1Mask|ControlMask,  XK_period, tagswapmon,        {.i = -1 } },
+	TAGKEYS(                        XK_1,                         0)
+	TAGKEYS(                        XK_2,                         1)
+	TAGKEYS(                        XK_3,                         2)
+	TAGKEYS(                        XK_4,                         3)
+	TAGKEYS(                        XK_5,                         4)
+	TAGKEYS(                        XK_6,                         5)
+	TAGKEYS(                        XK_7,                         6)
+	TAGKEYS(                        XK_8,                         7)
+	TAGKEYS(                        XK_9,                         8)
+	{ MODKEY|ShiftMask,             XK_q,      quit,              {0} },
+};
+
+/* button definitions */
+/* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
+static Button buttons[] = {
+	/* click                event mask         button          function        argument */
+	{ ClkLtSymbol,          0,                 Button1,        setlayout,      {0} },
+	{ ClkLtSymbol,          0,                 Button3,        setlayout,      {.v = &layouts[2]} },
+	{ ClkWinTitle,          0,                 Button2,        zoom,           {0} },
+	{ ClkStatusText,        0,                 Button2,        spawn,          {.v = termcmd } },
+	{ ClkClientWin,         MODKEY,            Button1,        movemouse,      {0} },
+	{ ClkClientWin,         MODKEY|Mod1Mask,   Button2,        togglefloating, {0} },
+	{ ClkClientWin,         MODKEY,            Button3,        resizemouse,    {0} },
+	{ ClkClientWin,         MODKEY,            Button4,        rotatestack,    {.i = +1 } },
+	{ ClkClientWin,         MODKEY,            Button5,        rotatestack,    {.i = -1 } },
+	{ ClkClientWin,         MODKEY,            Button2,        zoom,           {0} },
+	{ ClkClientWin,         MODKEY|Mod1Mask,   Button4,        cyclelayout,    {.i = -1 } },
+	{ ClkClientWin,         MODKEY|Mod1Mask,   Button5,        cyclelayout,    {.i = +1 } },
+	{ ClkTagBar,            0,                 Button1,        view,           {0} },
+	{ ClkTagBar,            0,                 Button3,        toggleview,     {0} },
+	{ ClkTagBar,            MODKEY,            Button1,        tag,            {0} },
+	{ ClkTagBar,            MODKEY,            Button3,        toggletag,      {0} },
+};
